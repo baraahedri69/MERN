ef9f4fdfb77a0df5ebef7de6d3d0c33ee0190588
unknown <baraahedri69@gmail.com>
2024-06-04 14:11:26 +0100

    super ninja

---
+++ Javascript/OOP/Core/Super Ninja/script.js
@@ -0,0 +1,40 @@
+class Ninja {
+    constructor(name, health = 100, speed = 3, strength = 3) {
+        this.name = name;
+        this.health = health;
+        this.speed = speed;
+        this.strength = strength;
+    }
+
+    sayName() {
+        console.log(`Name: ${this.name}`);
+    }
+
+    showStats() {
+        console.log(`Name: ${this.name}, Health: ${this.health}, Speed: ${this.speed}, Strength: ${this.strength}`);
+    }
+
+    drinkSake() {
+        this.health += 10;
+        console.log(`${this.name} drank sake and gained 10 health!`);
+    }
+}
+
+class Sensei extends Ninja {
+    constructor(name) {
+        super(name, 200, 10, 10);
+        this.wisdom = 10;
+    }
+
+    speakWisdom() {
+        this.drinkSake();
+        console.log("What one programmer can do in one month, two programmers can do in two months.");
+    }
+}
+
+// Example output
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+// -> "What one programmer can do in one month, two programmers can do in two months."
+superSensei.showStats();
+// -> "Name: Master Splinter, Health: 210, Speed: 10, Strength: 10"
