873479326a18f91e935643fb6701c9e99e70883d
unknown <baraahedri69@gmail.com>
2024-06-11 20:33:23 +0100

    prop it up

---
+++ React/Components/Practice/prop-it-up/src/PersonCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PersonCard = (props) => {
+    return (
+        <div>
+            <h2>{props.lastName}, {props.firstName}</h2>
+            <p>Age: {props.age}</p>
+            <p>Hair Color: {props.hairColor}</p>
+        </div>
+    );
+};
+
+export default PersonCard;
