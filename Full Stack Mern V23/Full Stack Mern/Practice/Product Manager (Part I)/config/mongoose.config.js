bd44a8bb1ae7bc392ba80aeec6940197d24d2319
Baraa Hedri <144041628+baraahedri69@users.noreply.github.com>
2024-06-21 14:33:25 +0100

    Delete Full Stack Mern V23/Full Stack Mern/Practice/Product Manager (Part I) directory

--- Full Stack Mern V23/Full Stack Mern/Practice/Product Manager (Part I)/config/mongoose.config.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const dbName = "products2";
-mongoose.connect(`mongodb://localhost/${dbName}`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(()=>console.log(`Connected to ${dbName} database!`))
-.catch((err)=>console.log(err));
--- Full Stack Mern V23/Full Stack Mern/Practice/Product Manager (Part I)/routes/product.routes.js
+++
@@ -1,6 +0,0 @@
-const ProductController = require("../controllers/product.controller"); 
-
-module.exports = (app)=>{
-
-    app.post("/api/products", ProductController.createProduct);
-}   
